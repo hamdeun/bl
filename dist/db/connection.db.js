72ee10b98df039d3a985d8431e3bd7aa886f4d9d
hamdeun <77253291+hamdeun@users.noreply.github.com>
2023-11-09 15:12:29 +0100

    Update connection.db.js

--- dist/db/connection.db.js
+++ dist/db/connection.db.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize"); //Import sequealize para nuestra base de datos
 //Creamos una constante con los valores de nuestra base de datos
-const sequelize = new sequelize_1.Sequelize('angulardb', 'root', '', {
-    host: 'localhost',
+const sequelize = new sequelize_1.Sequelize('begztetkugdcd9yotlkt', 'uriykddvnoclp6rj', 'eMsp0NpQ3nAllpaonPoC', {
+    host: 'begztetkugdcd9yotlkt-mysql.services.clever-cloud.com',
     dialect: 'mysql'
 });
 exports.default = sequelize;
